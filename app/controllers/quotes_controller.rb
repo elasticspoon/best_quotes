31c5ad470b04c17f34af6211a777d3930e3de6c7
Yuri Bocharov <quesadillaman@gmail.com>
2022-10-24 21:40:15 -0400

    c5 done

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,4 @@
+require 'uri'
 class QuotesController < Rulers::Controller
   def a_quote
     @test = 'crikey mate!'
@@ -27,5 +28,49 @@ class QuotesController < Rulers::Controller
 
     m = FileModel.create attrs
     render :quote, obj: m
+  end
+
+  def update_quote
+    return '' unless env['REQUEST_METHOD'] == 'POST'
+    file = FileModel.find(1)
+    file["quote"] = ('a'..'z').to_a.shuffle[0,8].join
+    file.save
+    ''
+  end
+
+  def submitter_index
+    submitters = FileModel.all.map { |model| model['submitter'] }.uniq
+
+    render :submitter_index, submitters: submitters
+  end
+
+  def submitter_quotes
+    submitters = query_params['submitter']
+    return index unless submitters
+
+    quotes = submitters.inject([]) do |arr, val| 
+      arr + FileModel.find_all_by_submitter(val) 
+    end.compact
+
+    render :index, quotes: quotes
+  end
+
+  private
+
+  def submitters_list
+    all.map { |model| model['submitter'] }.uniq
+  end
+
+  def query_params
+    return {} unless env['QUERY_STRING']
+    inputs = env['QUERY_STRING']&.split('&')
+
+    inputs.inject({}) do |hash, pair| 
+      k, v = pair.split('=')
+      puts CGI.unescape(v).inspect
+      (hash[k] ||= []) << CGI.unescape(v)
+      hash
+    end    
+    # Hash[URI.decode_www_form(env['QUERY_STRING'])]
   end
 end
